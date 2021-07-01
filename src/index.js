113f3c8f3756ed26f8df29d42da8d62692f38788
wendy <wendy.torres.19ee@gmail.com>
2021-06-30 23:26:54 -0300

    ajustes de index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+app.set("port",process.env.PORT || 3000)
 
-app.listen(3000, () => console.log("server on http://localhost3000")); 
+app.listen(app.get ("port"),() => console.log("Server Start http://localhost"+app.get("port")))
 
 app.use(express.static(path.join(__dirname,"../public")))
 app.use(require('./routes/web'))
